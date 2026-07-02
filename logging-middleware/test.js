2f210e358a7e194d62443a828e87e1f59885e844
MahaManisha <mahamanisha2005@gmail.com>
2026-07-02 11:47:58 +0530

    Completed logging middleware

---
+++ logging-middleware/test.js
@@ -0,0 +1,14 @@
+const { Log } = require("./index");
+
+async function testLogger() {
+    const result = await Log(
+        "backend",
+        "info",
+        "handler",
+        "Logger test successful"
+    );
+
+    console.log(result);
+}
+
+testLogger();
